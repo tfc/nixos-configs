a27ea28de2503e688f05e090f79163ec5aa2b63f
Jacek Galowicz <jacek@galowicz.de>
2022-03-18 09:08:16 +0100

    Allow for reboots after updates

--- flake.nix
+++ flake.nix
@@ -160,6 +160,7 @@
           system.autoUpgrade = {
             enable = true;
             flake = "github:tfc/nixos-configs";
+            allowReboot = true;
             flags = [
               " --no-write-lock-file"
             ];
