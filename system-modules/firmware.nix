8e47227419ee6674834124d67e1cb23c7c2aa3b1
Jacek Galowicz <jacek@galowicz.de>
2022-05-06 10:37:01 +0200

    Make firmware module work for aarch64, too

--- system-modules/firmware.nix
+++ system-modules/firmware.nix
@@ -1,7 +1,8 @@
-_:
+{ pkgs, ... }:
 
 {
-  hardware.cpu.intel.updateMicrocode = true;
+  hardware.cpu.intel.updateMicrocode = pkgs.stdenv.isx86_64;
   hardware.enableAllFirmware = true;
   hardware.enableRedistributableFirmware = true;
+  nixpkgs.config.allowUnfree = true; # needed by the unfree firmware blobs
 }
