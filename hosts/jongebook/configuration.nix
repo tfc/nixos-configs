337babbccc9802aabf9ed0ff9443697574e29356
Jacek Galowicz <jacek@galowicz.de>
2022-11-13 08:27:31 +0100

    Set configuration limit to 3

--- hosts/jongebook/configuration.nix
+++ hosts/jongebook/configuration.nix
@@ -6,6 +6,7 @@
   ];
 
   boot.loader.systemd-boot.enable = true;
+  boot.loader.systemd-boot.configurationLimit = 3;
   boot.loader.efi.canTouchEfiVariables = true;
   boot.loader.efi.efiSysMountPoint = "/boot/efi";
 
