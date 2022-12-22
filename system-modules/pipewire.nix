cd333062d20b89ae6519f888f901af6c07c5a999
Jacek Galowicz <jacek@galowicz.de>
2022-12-22 14:10:43 +0100

    audio file

--- system-modules/pipewire.nix
+++ system-modules/pipewire.nix
@@ -5,11 +5,10 @@
   security.rtkit.enable = true;
   services.pipewire = {
     enable = true;
+    audio.enable = true;
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
 
     # use the example session manager (no others are packaged yet so this is enabled by default,
     # no need to redefine it in your config for now)
