a67263ce90d785f13baf5cb0401871ac1b5b95fa
Jacek Galowicz <jacek@galowicz.de>
2022-12-23 07:51:35 +0100

    Remove redundant config lines and unused plugins

--- home-manager-modules/vim.nix
+++ home-manager-modules/vim.nix
@@ -1,20 +1,5 @@
 { pkgs, lib, agenix, hostName, ... }:
 
-let
-  vim-cmake = pkgs.vimUtils.buildVimPluginFrom2Nix {
-    pname = "vim-cmake";
-    version = "1.0";
-    src = pkgs.fetchFromGitHub {
-      owner = "vhdirk";
-      repo = "vim-cmake";
-      rev = "d4a6d1836987b933b064ba8ce5f3f0040a976880";
-      sha256 = "sha256-/JcO2gCJg99di33d0ANnL33F3yUeeBg4eBVA21iZCvY=";
-    };
-    postPatch = ''
-      sed -i 's;executable("cmake");executable("${pkgs.cmake}/bin/cmake");' plugin/cmake.vim
-    '';
-  };
-in
 {
   programs.neovim = {
     enable = true;
@@ -23,16 +8,10 @@ in
     extraConfig = builtins.readFile ./vimrc;
     plugins = with pkgs.vimPlugins; [
       bufexplorer
-      ctrlp
-      emodeline
-      fugitive
       haskell-vim
-      neomake
       nerdtree
       purescript-vim
-      surround
       vim-airline
-      vim-cmake
       vim-colorschemes
       vim-nix
       vim-tmux-navigator
