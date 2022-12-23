2b56c90e4fa9d0965c545b937a465833d523ef62
Jacek Galowicz <jacek@galowicz.de>
2022-12-23 11:04:15 +0100

    Modularize vim plugin settings

--- home-manager-modules/vim.nix
+++ home-manager-modules/vim.nix
@@ -4,22 +4,76 @@
   programs.neovim = {
     enable = true;
     vimAlias = true;
+    viAlias = true;
     vimdiffAlias = true;
-    extraConfig = builtins.readFile ./vimrc;
+    withNodeJs = true;
+    extraConfig = builtins.readFile ./vim-configs/vimrc;
     plugins = with pkgs.vimPlugins; [
-      bufexplorer
-      haskell-vim
-      nerdtree
+      {
+        plugin = bufexplorer;
+        config = "nnoremap <silent> <F4> :ToggleBufExplorer<CR>";
+      }
+      {
+        plugin = haskell-vim;
+        config = ''
+          let g:haskell_classic_highlighting = 1
+          let g:haskell_indent_if = 2
+          let g:haskell_indent_case = 2
+          let g:haskell_indent_let = 2
+          let g:haskell_indent_where = 2
+          let g:haskell_indent_before_where = 2
+          let g:haskell_indent_after_bare_where = 2
+          let g:haskell_indent_do = 2
+          let g:haskell_indent_in = 1
+          let g:haskell_indent_guard = 2
+          let g:haskell_indent_case_alternative = 1
+          let g:cabal_indent_section = 2
+        '';
+      }
+      {
+        plugin = nerdtree;
+        config = ''
+          autocmd StdinReadPre * let s:std_in=1
+          autocmd VimEnter * if argc() == 0 && !exists("s:std_in") | NERDTree | endif
+
+          let g:NERDTreeDirArrows = 1
+          let g:NERDTreeMinimalUI = 1
+          let g:NERDTreeMouseMode = 2
+          let g:NERDTreeQuitOnOpen = 1
+          let g:NERDTreeWinSize = 60
+
+          nnoremap <silent> <F3> :NERDTreeToggle<CR>
+        '';
+      }
+      nvim-lspconfig
+      plenary-nvim # dep of haskell-tools.nvim
       purescript-vim
-      vim-airline
+      {
+        plugin = vim-airline;
+        config = ''
+          set laststatus=2
+        '';
+      }
       vim-colorschemes
       vim-nix
       vim-tmux-navigator
-      vim-trailing-whitespace
+      {
+        plugin = vim-better-whitespace;
+        config = ''
+          let g:better_whitespace_enabled=1
+          let g:strip_whitespace_on_save=1
+          let g:strip_whitespace_confirm=0
+        '';
+      }
+      {
+        plugin = haskell-tools-nvim;
+        config = ":luafile ${./vim-configs/haskell-lsp.lua}";
+      }
     ];
   };
 
   home.packages = with pkgs; [
     rnix-lsp
+    nodePackages_latest.bash-language-server
   ];
 }
