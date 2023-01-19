d921f4edcd82af695b03b3c2e3acb37b4ca056e2
Jacek Galowicz <jacek@galowicz.de>
2023-01-19 10:58:10 +0100

    Switch from power-profiles-daemon to TLP

--- hosts/jongepad/configuration.nix
+++ hosts/jongepad/configuration.nix
@@ -53,6 +53,15 @@
   };
 
   powerManagement.powertop.enable = true;
-  services.power-profiles-daemon.enable = true;
   services.thermald.enable = true;
+  services.power-profiles-daemon.enable = false;
+  services.tlp = {
+    enable = true;
+    settings = {
+      CPU_BOOST_ON_AC = 1;
+      CPU_BOOST_ON_BAT = 0;
+      CPU_SCALING_GOVERNOR_ON_AC = "performance";
+      CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+    };
+  };
 }
