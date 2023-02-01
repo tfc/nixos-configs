38eb533dec19c80b27fab25101a69f5da42c26ad
Jacek Galowicz <jacek@galowicz.de>
2023-02-01 08:35:45 +0100

    Use ondemand governor when on battery

--- hosts/jongepad/configuration.nix
+++ hosts/jongepad/configuration.nix
@@ -61,7 +61,7 @@
       CPU_BOOST_ON_AC = 1;
       CPU_BOOST_ON_BAT = 0;
       CPU_SCALING_GOVERNOR_ON_AC = "performance";
-      CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+      CPU_SCALING_GOVERNOR_ON_BAT = "ondemand";
     };
   };
 }
