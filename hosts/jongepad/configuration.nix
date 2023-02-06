2eb629958f4b3e18a9835db8bec258b82c655a50
Jacek Galowicz <jacek@galowicz.de>
2023-02-06 12:24:51 +0100

    Drop thermald again

--- hosts/jongepad/configuration.nix
+++ hosts/jongepad/configuration.nix
@@ -53,15 +53,4 @@
   };
 
   powerManagement.powertop.enable = true;
-  services.thermald.enable = true;
-  services.power-profiles-daemon.enable = false;
-  services.tlp = {
-    enable = true;
-    settings = {
-      CPU_BOOST_ON_AC = 1;
-      CPU_BOOST_ON_BAT = 0;
-      CPU_SCALING_GOVERNOR_ON_AC = "performance";
-      CPU_SCALING_GOVERNOR_ON_BAT = "ondemand";
-    };
-  };
 }
