f695fd1f9d9599d4c8aa7f015aa2eb6486833477
Jacek Galowicz <jacek@galowicz.de>
2023-02-06 12:24:14 +0100

    Drop docker stuff from virtualization.nix

--- system-modules/virtualization.nix
+++ system-modules/virtualization.nix
@@ -5,18 +5,10 @@
   boot.extraModprobeConfig = "options kvm-intel nested=Y";
 
   environment.systemPackages = with pkgs; [
-    arion
-    docker-client # arion uses this to talk to podman
     libvirt
     virt-manager
     gnome.gnome-boxes
   ];
-
-  virtualisation.docker.enable = false;
-  virtualisation.podman.enable = true;
-  virtualisation.podman.dockerSocket.enable = true;
-  virtualisation.podman.defaultNetwork.dnsname.enable = true;
-  users.extraUsers.tfc.extraGroups = [ "podman" ];
 
   virtualisation.libvirtd.enable = true;
 }
