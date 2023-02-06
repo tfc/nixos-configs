f4f93c0d60907c52ba01c953a1d986b302fefb36
Jacek Galowicz <jacek@galowicz.de>
2023-02-06 12:25:09 +0100

    Add docker sysmodule

---
+++ system-modules/docker.nix
@@ -0,0 +1,12 @@
+_:
+
+{
+  virtualisation.docker.rootless = {
+    enable = true;
+    setSocketVariable = true;
+    daemon.settings = {
+      registry-mirrors = ["http://192.168.1.239:5001" "https://mirror.gcr.io" ];
+      insecure-registries = ["192.168.1.239:5001"];
+    };
+  };
+}
