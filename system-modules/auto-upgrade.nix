7cf1e6e95a0f12a8f21505ebbc9fa7302c8e9dfd
Jacek Galowicz <jacek@galowicz.de>
2023-02-07 12:35:06 +0100

    Add allowReboot with reboot window

--- system-modules/auto-upgrade.nix
+++ system-modules/auto-upgrade.nix
@@ -4,7 +4,12 @@
   system.autoUpgrade = {
     enable = true;
     flake = "github:tfc/nixos-configs";
-    allowReboot = true;
     flags = [ " --no-write-lock-file" ];
+    allowReboot = true;
+    dates = "02:00";
+    rebootWindow = {
+      lower = "02:00";
+      upper = "05:00";
+    };
   };
 }
