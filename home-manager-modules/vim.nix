238b0ed0d48bba007a735c5fd339ce47b6373f65
Jacek Galowicz <jacek@galowicz.de>
2023-02-27 10:56:57 +0100

    Allow whitespace fix for markdown

--- home-manager-modules/vim.nix
+++ home-manager-modules/vim.nix
@@ -74,9 +74,12 @@
       {
         plugin = vim-better-whitespace;
         config = ''
-          let g:better_whitespace_enabled=1
-          let g:strip_whitespace_on_save=1
-          let g:strip_whitespace_confirm=0
+          let g:better_whitespace_enabled = 1
+          let g:strip_whitespace_on_save = 1
+          let g:strip_whitespace_confirm = 0
+          let g:strip_whitelines_at_eof = 1
+          let g:show_spaces_that_precede_tabs = 1
+          let g:better_whitespace_filetypes_blacklist = ['diff', 'git', 'gitcommit', 'unite', 'qf', 'help', 'fugitive']
         '';
       }
     ];
