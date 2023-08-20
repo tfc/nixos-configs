4b089484bccbe66cabdc5311b2f87d558f11aead
Jacek Galowicz <jacek@galowicz.de>
2023-08-20 08:45:51 +0200

    Add remote-builds system module

---
+++ system-modules/remote-builds.nix
@@ -0,0 +1,16 @@
+{ ... }:
+{
+  nix.buildMachines = [ {
+   hostName = "build01.nix-consulting.de";
+   system = "x86_64-linux";
+   protocol = "ssh-ng";
+   maxJobs = 4;
+   speedFactor = 2;
+   supportedFeatures = [ "benchmark" "big-parallel" ];
+   mandatoryFeatures = [ ];
+  }] ;
+  nix.distributedBuilds = true;
+  nix.extraOptions = ''
+    builders-use-substitutes = true
+  '';
+}
