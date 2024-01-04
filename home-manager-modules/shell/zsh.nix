626e05e9155708cff63f1427b89afd35fef00418
Jacek Galowicz <tfc@jongebook.fritz.box>
2024-01-04 11:58:42 +0100

    drop zsh clutter

--- home-manager-modules/shell/zsh.nix
+++ home-manager-modules/shell/zsh.nix
@@ -4,16 +4,6 @@
   programs.zsh = {
     enable = true;
     enableCompletion = true;
-    #enableAutosuggestions = true;
-    #oh-my-zsh = {
-    #  enable = true;
-    #  plugins = [
-    #    "git"
-    #    "history"
-    #    "last-working-dir"
-    #  ];
-    #  theme = "lambda";
-    #};
     shellAliases = import ./common_aliases.nix;
   };
 }
