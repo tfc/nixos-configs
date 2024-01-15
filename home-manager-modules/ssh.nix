1731ab8e172540eaa273ce88403dcfff768b9344
Jacek Galowicz <jacek@galowicz.de>
2024-01-15 21:12:44 +0100

    Also add agent forwarding to trusted hosts

--- home-manager-modules/ssh.nix
+++ home-manager-modules/ssh.nix
@@ -1,8 +1,14 @@
-{ lib, ... }:
+{ lib, pkgs, ... }:
 let
   hostPart = x: builtins.elemAt (lib.strings.splitString "." x) 0;
   hostPartMapping = urls:
-    builtins.listToAttrs (map (x: { name = hostPart x; value = { hostname = x; }; }) urls);
+    builtins.listToAttrs (map (x: {
+      name = hostPart x;
+      value = {
+        hostname = x;
+        forwardAgent = true;
+      };
+    }) urls);
 in
 {
   programs.ssh = {
