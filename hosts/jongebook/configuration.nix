e048debf0ac13dce2634d1a63e954192e29177bd
Jacek Galowicz <jacek@galowicz.de>
2024-02-15 12:41:05 +0700

    Increase cores of linux builder

--- hosts/jongebook/configuration.nix
+++ hosts/jongebook/configuration.nix
@@ -14,13 +14,14 @@ in
   nix.linux-builder = {
     enable = true;
     ephemeral = true;
+    maxJobs = 4;
     config = {
       virtualisation = {
         darwin-builder = {
           diskSize = 40 * 1024;
           memorySize = 8 * 1024;
         };
-        cores = 4;
+        cores = 6;
       };
     };
   };
