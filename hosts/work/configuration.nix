97e8afe2733594e770a1bb601afd5181d4f27b04
Jacek Galowicz <jacek@galowicz.de>
2024-09-04 10:27:13 +0200

    Add Nvidia module

--- hosts/work/configuration.nix
+++ hosts/work/configuration.nix
@@ -4,18 +4,21 @@
   imports = [
     ./hardware-configuration.nix
     ./ai.nix
+    ./nvidia.nix
   ];
 
   boot.loader.systemd-boot.enable = true;
+  boot.loader.systemd-boot.configurationLimit = 3;
   networking.hostName = "ai";
   networking.networkmanager.enable = true;
+
+  powerManagement.cpuFreqGovernor = "performance";
 
   time.timeZone = "Europe/Berlin";
 
   i18n.defaultLocale = "en_US.UTF-8";
 
   services.xserver.enable = true;
-  #services.xserver.videoDrivers = [ "nvidia" ];
   services.xserver.desktopManager.gnome.enable = true;
   services.xserver.displayManager.gdm.enable = true;
 
@@ -40,19 +43,7 @@
 
   services.openssh.enable = true;
 
-  boot.kernelPackages = pkgs.linuxPackages_latest;
   nixpkgs.config.allowUnfree = true;
   hardware.enableAllFirmware = true;
   system.stateVersion = "24.11";
-
-  hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.latest;
-
-  services.xserver.videoDrivers = ["nvidia"];
-
-  hardware.graphics.enable = true;
-  hardware.nvidia.modesetting.enable = true;
-  hardware.nvidia.powerManagement.enable = false;
-  hardware.nvidia.powerManagement.finegrained = false;
-  hardware.nvidia.open = false;
-  hardware.nvidia.nvidiaSettings = true;
 }
---
+++ hosts/work/nvidia.nix
@@ -0,0 +1,39 @@
+{ pkgs, lib, config, ... }:
+
+{
+  nixpkgs.overlays = [
+    (final: prev: {
+      linuxPackages_latest = prev.linuxPackages_latest.extend (kernelFinal: kernelSuper: {
+        nvidiaPackages = kernelSuper.nvidiaPackages // {
+          nvidia_testing = kernelSuper.nvidiaPackages.mkDriver {
+            version = "560.35.03";
+            sha256_32bit = "sha256-8pMskvrdQ8WyNBvkU/xPc/CtcYXCa7ekP73oGuKfH+M=";
+            sha256_64bit = "sha256-8pMskvrdQ8WyNBvkU/xPc/CtcYXCa7ekP73oGuKfH+M=";
+            settingsSha256 = "sha256-kQsvDgnxis9ANFmwIwB7HX5MkIAcpEEAHc8IBOLdXvk=";
+            sha256_aarch64 = lib.fakeHash;
+            openSha256 = lib.fakeHash;
+            persistencedSha256 = lib.fakeHash;
+          };
+        };
+      });
+    })
+  ];
+
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+
+  services.xserver.videoDrivers = ["nvidia"];
+
+  hardware.nvidia = {
+    modesetting.enable = true;
+    powerManagement.enable = false;
+    powerManagement.finegrained = false;
+    open = false;
+    nvidiaSettings = true;
+    package = config.boot.kernelPackages.nvidiaPackages.nvidia_testing;
+
+  };
+
+  boot.blacklistedKernelModules = [ "nouveau" ];
+
+  hardware.graphics.enable = true;
+}
