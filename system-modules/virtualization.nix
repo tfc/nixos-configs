58b1a068f1716c54c86c470d8e7f7557b67773fe
Jacek Galowicz <jacek@galowicz.de>
2024-10-05 18:03:52 +0200

    Add libvirtd with efi

--- system-modules/virtualization.nix
+++ system-modules/virtualization.nix
@@ -10,6 +10,21 @@
     gnome-boxes
   ];
 
-  virtualisation.libvirtd.enable = true;
-  #virtualisation.docker.enable = true;
+  virtualisation.libvirtd = {
+    enable = true;
+    qemu = {
+      package = pkgs.qemu_kvm;
+      runAsRoot = true;
+      swtpm.enable = true;
+      ovmf = {
+        enable = true;
+        packages = [
+          (pkgs.OVMF.override {
+            secureBoot = true;
+            tpmSupport = true;
+          }).fd
+        ];
+      };
+    };
+  };
 }
