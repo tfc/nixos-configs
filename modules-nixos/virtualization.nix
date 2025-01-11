7c97e4eff9c55d9bd99bbed6df97a0109c8c28ed
Jacek Galowicz <jacek@galowicz.de>
2025-01-11 14:22:44 +0100

    generalize virtualization module a bit

--- modules-nixos/virtualization.nix
+++ modules-nixos/virtualization.nix
@@ -1,8 +1,24 @@
-{ pkgs, ... }:
 {
-  boot.binfmt.emulatedSystems = [ "aarch64-linux" ];
+  pkgs,
+  lib,
+  config,
+  ...
+}:
+let
+  inherit (config.nixpkgs) hostPlatform;
+in
+{
+  boot.binfmt.emulatedSystems =
+    lib.optional (!hostPlatform.isx86_64) "x86_64-linux"
+    ++ lib.optional (!hostPlatform.isAarch64) "aarch64-linux";
 
-  boot.extraModprobeConfig = "options kvm-intel nested=Y";
+  boot.extraModprobeConfig =
+    if config.hardware.cpu.intel.updateMicrocode then
+      "options kvm-intel nested=Y"
+    else if config.hardware.cpu.amd.updateMicrocode then
+      "options kvm-amd nested=1"
+    else
+      "";
 
   environment.systemPackages = with pkgs; [
     libvirt
