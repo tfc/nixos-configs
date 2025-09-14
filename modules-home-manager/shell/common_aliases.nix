da8b448a1762c88b89b90bb6ac5e3b6d9c3db062
Jacek Galowicz <jacek@galowicz.de>
2025-09-14 12:46:19 +0200

    add npu alias

--- modules-home-manager/shell/common_aliases.nix
+++ modules-home-manager/shell/common_aliases.nix
@@ -1,6 +1,7 @@
 {
   ".." = "cd ..";
-  ll = "ls -lsa";
-  nrn = "nix repl --file '<nixpkgs>'";
   fpi = "nix flake init -t github:hercules-ci/flake-parts";
+  ll = "ls -lsa";
+  npu = "NIXPKGS_ALLOW_UNFREE=1 nix profile upgrade --all --impure";
+  nrn = "nix repl --file '<nixpkgs>'";
 }
