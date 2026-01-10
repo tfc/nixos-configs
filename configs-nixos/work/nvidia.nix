0de1eb501dcdcf5fccbb960f35b1fec50d93f0d5
Jacek Galowicz <jacek@galowicz.de>
2026-01-11 00:43:21 +0100

    Add required mounts for cuda tests in sandbox

--- configs-nixos/work/nvidia.nix
+++ configs-nixos/work/nvidia.nix
@@ -17,4 +17,23 @@
   services.xserver.videoDrivers = [ "nvidia" ];
 
   hardware.graphics.enable = true;
+
+  programs.nix-required-mounts = {
+    enable = true;
+    presets.nvidia-gpu.enable = true;
+    # Workaround for <https://github.com/NixOS/nix/issues/9272>, copied from
+    # <https://github.com/nix-community/infra/pull/1807>.
+    extraWrapperArgs = [
+      "--run shift"
+      "--add-flag '${
+        builtins.unsafeDiscardOutputDependency
+          (derivation {
+            name = "needs-cuda";
+            builder = "_";
+            system = "_";
+            requiredSystemFeatures = [ "cuda" ];
+          }).drvPath
+      }'"
+    ];
+  };
 }
