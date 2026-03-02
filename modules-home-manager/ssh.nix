44e7d47741e2c44048d78524e822148d85c08f08
Jacek Galowicz <jacek@galowicz.de>
2026-03-02 16:01:52 +0100

    fix home manager ssh deprecation warning

--- modules-home-manager/ssh.nix
+++ modules-home-manager/ssh.nix
@@ -12,11 +12,26 @@ let
         };
       }) urls
     );
+  defaultAsterisk = { 
+    "*" = {
+	forwardAgent = false;
+	addKeysToAgent = "no";
+	compression = false;
+	serverAliveInterval = 0;
+	serverAliveCountMax = 3;
+	hashKnownHosts = false;
+	userKnownHostsFile = "~/.ssh/known_hosts";
+	controlMaster = "no";
+	controlPath = "~/.ssh/master-%r@%n:%p";
+	controlPersist = "no";
+    };
+  };
 in
 {
   programs.ssh = {
     enable = true;
-    matchBlocks = hostPartMapping [
+    enableDefaultConfig = false;
+    matchBlocks = defaultAsterisk // hostPartMapping [
       "server-sicherheit.lxht.de"
       "training-machine.nixcademy.com"
     ];
