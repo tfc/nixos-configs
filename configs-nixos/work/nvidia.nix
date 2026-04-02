958c3f648dcaf7fcee8ebcad6599672bb18dd4e8
Jacek Galowicz <jacek@galowicz.de>
2026-04-02 12:08:42 +0200

    drop not needed hook stuff

--- configs-nixos/work/nvidia.nix
+++ configs-nixos/work/nvidia.nix
@@ -21,19 +21,5 @@
   programs.nix-required-mounts = {
     enable = true;
     presets.nvidia-gpu.enable = true;
-    # Workaround for <https://github.com/NixOS/nix/issues/9272>, copied from
-    # <https://github.com/nix-community/infra/pull/1807>.
-    extraWrapperArgs = [
-      "--run shift"
-      "--add-flag '${
-        builtins.unsafeDiscardOutputDependency
-          (derivation {
-            name = "needs-cuda";
-            builder = "_";
-            system = "_";
-            requiredSystemFeatures = [ "cuda" ];
-          }).drvPath
-      }'"
-    ];
   };
 }
