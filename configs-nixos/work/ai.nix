6b049b5248488a551d521ef1c644d16fd30aa576
Jacek Galowicz <jacek@galowicz.de>
2026-04-11 18:34:01 +0200

    reenable ollama

--- configs-nixos/work/ai.nix
+++ configs-nixos/work/ai.nix
@@ -1,31 +1,10 @@
-{ config, ... }:
+{ pkgs, ... }:
 
 {
   services.ollama = {
     enable = true;
-    acceleration = "cuda";
+    package = pkgs.ollama-cuda;
   };
 
-  users.extraGroups.ai = { };
-  users.extraUsers.ai = {
-    isNormalUser = true;
-    description = "AI user";
-    group = "ai";
-    createHome = false;
-  };
-
-  services.postgresql = {
-    enable = true;
-    extensions = [
-      config.services.postgresql.package.pkgs.pgvector
-    ];
-    initialScript = builtins.toFile "postgres-initScript" ''
-      CREATE ROLE ai WITH LOGIN SUPERUSER;
-      CREATE DATABASE ai;
-      GRANT ALL PRIVILEGES ON DATABASE ai TO ai;
-
-      \c ai;
-      CREATE EXTENSION vector;
-    '';
-  };
+  services.nextjs-ollama-llm-ui.enable = true;
 }
