5b063c9ad53f53c34e707c9e9b615f91710e50cb
Jacek Galowicz <jacek@applicative.systems>
2026-05-03 14:11:21 +0200

    add background switcher

--- profiles-nixos/applicative-systems-customization.nix
+++ profiles-nixos/applicative-systems-customization.nix
@@ -14,6 +14,15 @@ let
     osName = "NixOS";
     osVersion = "2026";
   };
+
+  reset-background = pkgs.writeShellApplication {
+    name = "gnome-reset-background";
+    runtimeInputs = [ pkgs.glib ];
+    text = ''
+      gsettings reset org.gnome.desktop.background picture-uri
+      gsettings reset org.gnome.desktop.background picture-uri-dark
+    '';
+  };
 in
 {
   options.customization = {
@@ -37,6 +46,10 @@ in
         default = ./artwork/wallpaper-applicative-dark.webp;
         description = "Path to the wallpaper used with the dark color scheme";
       };
+      resetUserBackground = lib.mkEnableOption (
+        "resetting each user's GNOME background keys to the system default at session start, "
+        + "so per-user dconf overrides do not shadow the system-provided wallpaper"
+      ) // { default = true; };
     };
     plymouth-logo = {
       enable = lib.mkEnableOption "Plymouth icon customization" // { default = true; };
@@ -64,6 +77,18 @@ in
         picture-uri='file://${cfg.gnome-background.lightFile}'
         picture-uri-dark='file://${cfg.gnome-background.darkFile}'
       '';
+    })
+    (lib.mkIf (cfg.gnome-background.enable && cfg.gnome-background.resetUserBackground) {
+      systemd.user.services.gnome-reset-background = {
+        description = "Reset GNOME background to system default at login";
+        unitConfig.ConditionEnvironment = "XDG_CURRENT_DESKTOP=GNOME";
+        wantedBy = [ "graphical-session.target" ];
+        partOf = [ "graphical-session.target" ];
+        serviceConfig = {
+          Type = "oneshot";
+          ExecStart = lib.getExe reset-background;
+        };
+      };
     })
     (lib.mkIf cfg.plymouth-logo.enable {
       boot.plymouth = {
