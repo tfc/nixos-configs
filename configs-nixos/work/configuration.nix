24ca31af1630a9c964e498b3a2ecf12225bcca05
Jacek Galowicz <jacek@applicative.systems>
2026-05-09 14:51:52 +0200

    enable nested virt

--- configs-nixos/work/configuration.nix
+++ configs-nixos/work/configuration.nix
@@ -54,6 +54,7 @@
   boot.kernelParams = [ "iommu=pt" "pcie_aspm=off" ];
   boot.extraModprobeConfig = ''
     options rtl8821ae fwlps=0 swlps=0 ips=0 aspm=0 msi=1
+    options kvm_amd nested=1
   '';
 
   powerManagement.cpuFreqGovernor = "balanced";
