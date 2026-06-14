ecb33cddd0e0be1daba3e4b1bea11fc36c399266
Jacek Galowicz <jacek@applicative.systems>
2026-06-14 14:03:44 +0200

    fix deprecation warnings

--- modules-home-manager/ssh.nix
+++ modules-home-manager/ssh.nix
@@ -7,23 +7,23 @@ let
       map (x: {
         name = hostPart x;
         value = {
-          hostname = x;
-          forwardAgent = true;
+          HostName = x;
+          ForwardAgent = true;
         };
       }) urls
     );
   defaultAsterisk = {
     "*" = {
-      forwardAgent = false;
-      addKeysToAgent = "no";
-      compression = false;
-      serverAliveInterval = 0;
-      serverAliveCountMax = 3;
-      hashKnownHosts = false;
-      userKnownHostsFile = "~/.ssh/known_hosts";
-      controlMaster = "no";
-      controlPath = "~/.ssh/master-%r@%n:%p";
-      controlPersist = "no";
+      ForwardAgent = false;
+      AddKeysToAgent = "no";
+      Compression = false;
+      ServerAliveInterval = 0;
+      ServerAliveCountMax = 3;
+      HashKnownHosts = false;
+      UserKnownHostsFile = "~/.ssh/known_hosts";
+      ControlMaster = "no";
+      ControlPath = "~/.ssh/master-%r@%n:%p";
+      ControlPersist = "no";
     };
   };
 in
@@ -31,7 +31,7 @@ in
   programs.ssh = {
     enable = true;
     enableDefaultConfig = false;
-    matchBlocks =
+    settings =
       defaultAsterisk
       // hostPartMapping [
         "server-sicherheit.lxht.de"
