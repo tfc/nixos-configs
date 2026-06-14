b66cb14aed3952f995b4ca0a86346ba01a2dc287
Jacek Galowicz <jacek@applicative.systems>
2026-06-14 14:03:33 +0200

    add cuda mirrors

--- configs-nixos/work/configuration.nix
+++ configs-nixos/work/configuration.nix
@@ -161,4 +161,9 @@
     "h /data/video-cut   -    -   -     - +C"
   ];
 
+  nix.settings = {
+    substituters = [ "https://cache.nixos-cuda.org" ];
+    trusted-public-keys = [ "cache.nixos-cuda.org:74DUi4Ye579gUqzH4ziL9IyiJBlDpMRn9MBN8oNan9M=" ];
+  };
+
 }
